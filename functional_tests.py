bc939cc212b4263255d7667ce24745fb963bef09
Tobiasz Hołtyn <tho@hicron.com>
2017-10-05 10:08:34 +0200

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         # to check out its homepage
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1')
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         
         # She is invited to enter a to-do item straight away
@@ -36,7 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
             )
         
         self.fail('Finish the test!')
