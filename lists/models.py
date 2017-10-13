7e34f5e1a4d885efe9da90b895b94595ecf8c6ca
Tobiasz Hołtyn <tho@hicron.com>
2017-10-13 15:30:29 +0200

    items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+class List(models.Model):
+    pass
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
