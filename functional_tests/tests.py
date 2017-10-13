070ef4e5f3a71d6209264cd74d14e6784668cec1
Tobiasz Hołtyn <tho@hicron.com>
2017-10-13 17:44:32 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,7 +11,9 @@ from selenium import webdriver
 MAX_WAIT = 2
 
 def get_webdriver():
-    return webdriver.Chrome(executable_path='../../chromedriver.exe')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("test-type")
+    return webdriver.Chrome(executable_path='../../chromedriver.exe', chrome_options=chrome_options)
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -132,5 +134,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
         
-        time.sleep(1)
+        #time.sleep(1)
         # Satisfied, they both go back to sleep
+        
+        
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 
+                               512, delta=10)
+        
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
