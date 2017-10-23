99eb42cb2a2f67f73ee02c6fc7054f3d23c271f6
Tobiasz Hołtyn <tho@hicron.com>
2017-10-23 13:02:33 +0200

    Moved Fts into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,45 +1,9 @@
-import os
-import unittest
 from selenium.webdriver.common.keys import Keys
-import time
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.common.exceptions import WebDriverException
-cwd = os.getcwd()
-print(cwd)
-from selenium import webdriver
 
-MAX_WAIT = 2
+from .base import FunctionalTest
 
-def get_webdriver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("test-type")
-    return webdriver.Chrome(executable_path='../../chromedriver.exe', chrome_options=chrome_options)
+class NewVisitorTest(FunctionalTest):
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = get_webdriver()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-    def tearDown(self):
-        self.browser.quit()
-        
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-        
-
-    
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -112,13 +76,12 @@ class NewVisitorTest(StaticLiveServerTestCase):
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = get_webdriver()
+        self.browser = self.get_webdriver()
         # Francis visits the home page.  There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('make a fly', page_text)
         
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
@@ -140,23 +103,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         #time.sleep(1)
         # Satisfied, they both go back to sleep
         
-        
-    def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 
-                               512, delta=10)
-        
-        # She starts a new list and sees the input is nicely
-        # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
